36c626890261d4a5fd2dc3fc7ace69b0104a9692
Alexandre Batistella Bellas <alexandre.bellas@usp.br>
2023-07-07 13:37:40 -0300

    fix(provider): add missing config in provider

--- src/DuskApiConfServiceProvider.php
+++ src/DuskApiConfServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Manyapp\DuskApiConf;
 
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 
 class DuskApiConfServiceProvider extends ServiceProvider
@@ -13,20 +14,28 @@ class DuskApiConfServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/Routes/Route.php');
-        $this->loadViewsFrom(__DIR__.'/Resources/Views', 'duskapiconf');
-        
+        $this->loadRoutesFrom(__DIR__ . '/Routes/Route.php');
+        $this->loadViewsFrom(__DIR__ . '/Resources/Views', 'duskapiconf');
+
         $this->mergeConfigFrom(
-            __DIR__.'/../config/config.php',
+            __DIR__ . '/../config/config.php',
             'manyapp.duskapiconf'
         );
 
+        $contents = Storage::disk(config('manyapp.duskapiconf.disk'))->get(config('manyapp.duskapiconf.file'));
+        $decoded = json_decode($contents, true);
+        foreach (array_keys($decoded) as $k) {
+            config([$k => $decoded[$k]]);
+        }
+
         $this->publishes([
-            __DIR__.'/../config/config.php' => config_path('duskapiconf.php'),
+            __DIR__ . '/../config/config.php' => config_path('duskapiconf.php'),
         ]);
 
         $router = $this->app['router'];
-        $router->pushMiddlewareToGroup('web', \Manyapp\DuskApiConf\Middleware\ConfigStoreMiddleware::class);
+        $this->app->booted(function () use ($router) {
+            $router->pushMiddlewareToGroup('web', \Manyapp\DuskApiConf\Middleware\ConfigStoreMiddleware::class);
+        });
     }
 
     /**
